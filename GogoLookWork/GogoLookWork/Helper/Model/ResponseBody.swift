f260f7f75b6d04ed345297067a62a88133983e69
Jason shih <shi0jason@gmail.com>
2021-01-01 15:10:42 +0800

    Add Model

---
+++ GogoLookWork/GogoLookWork/Helper/Model/ResponseBody.swift
@@ -0,0 +1,21 @@
+//
+//  ResponseBody.swift
+//  GogoLookWork
+//
+//  Created by Jason on 2021/1/1.
+//  Copyright © 2021 Jason. All rights reserved.
+//
+
+struct ResponseBody: Codable {
+    let requestHash: String?
+    let requestCached: Bool?
+    let requestCacheExpiry: Int?
+    let top: [TopObject]
+    
+    enum CodingKeys: String, CodingKey {
+        case requestHash = "request_hash"
+        case requestCached = "request_cached"
+        case requestCacheExpiry = "request_cache_expiry"
+        case top
+    }
+}
---
+++ GogoLookWork/GogoLookWork/Helper/Model/TopObject.swift
@@ -0,0 +1,36 @@
+//
+//  TopObject.swift
+//  GogoLookWork
+//
+//  Created by Jason on 2021/1/1.
+//  Copyright © 2021 Jason. All rights reserved.
+//
+
+struct TopObject: Codable {
+
+    let id: Int
+    let rank: Int
+    let title: String
+    let url: String
+    let imageUrl: String
+    let type: String
+    let episodes: Int
+    let startDate: String
+    let endDate: String
+    let members: Int
+    let score: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "mal_id"
+        case rank
+        case title
+        case url
+        case imageUrl = "image_url"
+        case type
+        case episodes
+        case startDate = "start_date"
+        case endDate = "end_date"
+        case members
+        case score
+    }
+}
