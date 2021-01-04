2092ad85f00adadee01504efaaf4f85600553792
Jason shih <shi0jason@gmail.com>
2021-01-04 09:47:37 +0800

    add Favorite set

---
+++ GogoLookWork/GogoLookWork/Helper/DefaultHelper.swift
@@ -0,0 +1,34 @@
+//
+//  DefaultHelper.swift
+//  GogoLookWork
+//
+//  Created by Jason on 2021/1/3.
+//  Copyright © 2021 Jason. All rights reserved.
+//
+
+import Foundation
+class DefaultHelper {
+    
+    func setFlag(at id: Int) {
+        if let favoriteDict = UserDefaults.standard.dictionary(forKey: "Favorite") {
+            var dict = favoriteDict
+            if let idValue = dict["\(id)"] as? Bool {
+                dict["\(id)"] = idValue == true ? false : true
+                UserDefaults.standard.set(dict, forKey: "Favorite")
+            }
+        } else {
+            let dict = ["\(id)": true]
+            UserDefaults.standard.set(dict , forKey: "Favorite")
+        }
+    }
+    
+    func getFlag(at id: Int) -> Bool {
+        if let favoriteDict = UserDefaults.standard.dictionary(forKey: "Favorite") {
+            var dict = favoriteDict
+            if let idValue = dict["\(id)"] as? Bool {
+                return idValue
+            }
+        }
+        return false
+    }
+}
--- GogoLookWork/GogoLookWork/ViewController/WebController.swift
+++ GogoLookWork/GogoLookWork/ViewController/WebController.swift
@@ -2,8 +2,8 @@
 //  WebController.swift
 //  GogoLookWork
 //
-//  Created by 施崇邑 on 2021/1/3.
-//  Copyright © 2021 施崇邑. All rights reserved.
+//  Created by Jason on 2021/1/3.
+//  Copyright © 2021 Jason. All rights reserved.
 //
 
 import UIKit
